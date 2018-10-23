cc22e429055ad2b8263c0f8e45b1cbba7b8459ee
yj-wu16 <yj-wu16@mails.tsinghua.edu.cn>
2018-10-22 23:06:54 -0400

    a Point class

---
+++ Point.cpp
@@ -0,0 +1,17 @@
+
+#include "Point.h"
+
+Point::Point()
+{
+	dx = 0.0;
+}
+
+void Point::setdx(double dxx)
+{
+	dx = dxx ; 
+}
+
+double Point::getdx()
+{
+	return dx ;
+}
---
+++ Point.h
@@ -0,0 +1,16 @@
+#ifdef POINT_H
+#define POINT_H
+
+class Point
+{
+private:
+	double dx;
+public:
+	Point();
+	~Point();
+	void setdx( double dxx);
+	double getdx();
+};
+
+
+#endif
